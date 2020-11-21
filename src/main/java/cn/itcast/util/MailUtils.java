990e853045fe5b12e5be0659c42f46f4fa07835a
365318663 <45816584+365318663@users.noreply.github.com>
2020-11-21 15:54:18 +0800

    Update MailUtils.java

--- src/main/java/cn/itcast/util/MailUtils.java
+++ src/main/java/cn/itcast/util/MailUtils.java
@@ -10,7 +10,7 @@ import java.util.Properties;
  */
 public final class MailUtils {
     private static final String USER = "365318663@qq.com"; // 发件人称号，同邮箱地址
-    private static final String PASSWORD = "wvsvuezmlokzbjfg"; // 如果是qq邮箱可以使户端授权码，或者登录密码
+    private static final String PASSWORD = "" // 如果是qq邮箱可以使户端授权码，或者登录密码
 
     /**
      *
